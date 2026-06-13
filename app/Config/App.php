6e6b8c9b7a592aa9c888532e7821f55a701d3aa8
Andri Hendrawan <andrihendrawan0606@gmail.com>
2026-06-13 23:05:15 +0700

    update dikit

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost:8080/';
+    public string $baseURL = 'http://152.42.190.255/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
@@ -29,8 +29,7 @@ class App extends BaseConfig
      *
      * @var list<string>
      */
-    public array $allowedHostnames = [];
-
+    public array $allowedHostnames = ['152.42.190.255'];
     /**
      * --------------------------------------------------------------------------
      * Index File
